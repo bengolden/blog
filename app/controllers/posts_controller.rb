020d6c02ed1b45943c9229bdaa2e570c2fbe2c37
Ben Golden <bengolden1@gmail.com>
2014-06-01 22:01:26 -0500

    preparing to push

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,20 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    Post.create(user_id: params[:user_id], body: params[:body])
+    redirect_to "/users/#{params[:user_id]}"
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
